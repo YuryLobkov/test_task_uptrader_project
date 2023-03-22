8f507fd9b6cbe853bb341b19f2959e9db36c9c20
Yury Lobkov <racoon.enot@gmail.com>
2023-03-22 14:35:24 -0300

    debug tool

--- test_task_uptrader/urls.py
+++ test_task_uptrader/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('nested_menu.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
