4917542f6ca93dd3ddd9c18ce5a2afd5efe3dbb1
Yury Lobkov <racoon.enot@gmail.com>
2023-03-22 06:22:16 -0300

    3 level nested views for debuging menu

--- nested_menu/urls.py
+++ nested_menu/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('<item_name>', views.item_page, name='level1_page'),
+    path('<item_name>/<item_name2>', views.item_page_level2, name='level2_page'),
+    path('<item_name>/<item_name2>/<item_name3>', views.item_page_level3, name='level3_page'),
 ]
--- nested_menu/views.py
+++ nested_menu/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+# item_page views for rendering ierarchical urls up to 3 levels
+def item_page(request, item_name):
+    context = {'pagename': item_name}
+    return render(request, 'index.html', context=context)
+
+
+def item_page_level2(request, item_name, item_name2):
+    context = {'pagename': item_name, 'item_name2': item_name2}
+    return render(request, 'index.html', context=context)
+
+
+def item_page_level3(request, item_name, item_name2, item_name3):
+    context = {'pagename': item_name, 'item_name2': item_name2, 'item_name3': item_name3}
+    return render(request, 'index.html', context=context)
+
 
 def homepage(request):
     return render(request, 'index.html')
