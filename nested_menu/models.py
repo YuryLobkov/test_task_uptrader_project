e3436d89b3b1f18b251f33421a2a5288b631cb90
Yury Lobkov <racoon.enot@gmail.com>
2023-03-22 04:30:55 -0300

    model MenuItem create

--- nested_menu/models.py
+++ nested_menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def is_active(self, current_url):
+        return current_url == self.url
+
+    def __str__(self):
+        return self.name
