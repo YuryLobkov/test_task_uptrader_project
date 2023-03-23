08d96f113c966adf8ed8967ce9560ee4a8473393
Yury Lobkov <racoon.enot@gmail.com>
2023-03-22 23:00:04 -0300

    redone func to only 1 sql hit (full unfolded menu)

--- nested_menu/templatetags/draw_menu.py
+++ nested_menu/templatetags/draw_menu.py
@@ -8,26 +8,39 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def draw_menu(context, menu_name):
     current_url = context['request'].path
-    menu_items = MenuItem.objects.filter(
-        parent__isnull=True, name=menu_name).prefetch_related('children') # getting root item (menu name)
-    menu_html = recursive_draw(menu_items, current_url)
-    return format_html(menu_html)
+    all_items = MenuItem.objects.all().select_related(
+        'parent')  # every item in database
+    root_items = []
+    for item in all_items:
+        if current_url.startswith(item.url) and item.parent == None:
+            root_item_of_active = item
+        if item.url == current_url:
+            current_item = item
+        if item.parent == None:
+            root_items.append(item)
+        if item.name == menu_name:
+            selected_menu = item
+
+    output_html = f'<ul> <li> <a href="{selected_menu.url}">{selected_menu.name}</a>'
+    output_html += draw_childs(selected_menu, all_items)
+    output_html += ' </li> </ul>'
+    return format_html(output_html)
 
 
-def recursive_draw(items, current_url, selected=False):
-    menu_html = '<ul>'
-    for item in items:
-        if current_url.startswith(item.url): # check if selected item is child of this item (or is selected itself)
-            selected = True
-        if item.is_active(current_url): # if current url is url in current item, set css class "active"
-            active_class = 'active'
-        else:
-            active_class = ''
-        menu_html += f'<li class="{active_class}"><a href="{item.url}">{item.name}</a>'
-        children = item.children.all() # get all childrens of current item to further render
-        if children and selected: # check if there are childrens and if they are 1st level nested (to satisfy task point #2)
-            menu_html += recursive_draw(children, current_url)
-            selected = False # drop selected flag not to render non-related items with selected item
-        menu_html += '</li>'
-    menu_html += '</ul>'
-    return menu_html
+def draw_childs(parent, all_items):
+    child_tree = '<ul>'
+    for item in all_items:
+        if item.parent == parent:
+            child_tree += f'<li><a href="{item.url}">{item.name}</a>'
+            if check_childs(item, all_items):
+                child_tree += draw_childs(item, all_items)
+            child_tree += '</li>'
+    child_tree += '</ul>'
+    return child_tree
+
+
+def check_childs(item_to_check, all_items):
+    for item in all_items:
+        if item.parent == item_to_check:
+            return True
+    return False
