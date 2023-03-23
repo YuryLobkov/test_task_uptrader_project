cda01b52787da06758f38a62296a3c08cf44e988
Yury Lobkov <racoon.enot@gmail.com>
2023-03-22 23:43:13 -0300

    cleaning code, comments

--- nested_menu/templatetags/draw_menu.py
+++ nested_menu/templatetags/draw_menu.py
@@ -10,14 +10,7 @@ def draw_menu(context, menu_name):
     current_url = context['request'].path
     all_items = MenuItem.objects.all().select_related(
         'parent')  # every item in database
-    root_items = []
     for item in all_items:
-        if current_url.startswith(item.url) and item.parent == None:
-            root_item_of_active = item
-        if item.url == current_url:
-            current_item = item
-        if item.parent == None:
-            root_items.append(item)
         if item.name == menu_name:
             selected_menu = item
 
@@ -28,20 +21,22 @@ def draw_menu(context, menu_name):
 
 
 def draw_childs(parent, all_items, current_url):
-    if not current_url.startswith(parent.url):
+    # check if needed to unfold menu item
+    if not current_url.startswith(parent.url): 
         return ''
-        
+    # render child element
     child_tree = '<ul>'
     for item in all_items:
         if item.parent == parent:
             child_tree += f'<li><a href="{item.url}">{item.name}</a>'
+            # if this element has childs, recursive render
             if check_childs(item, all_items):
                 child_tree += draw_childs(item, all_items, current_url)
             child_tree += '</li>'
     child_tree += '</ul>'
     return child_tree
 
-
+# check childs func to avoid additional db hits
 def check_childs(item_to_check, all_items):
     for item in all_items:
         if item.parent == item_to_check:
