3033da3ee8e81c1873de6d5724db3c200e0c44da
Yury Lobkov <racoon.enot@gmail.com>
2023-03-23 00:17:49 -0300

    model change to satisfy rule#7

--- nested_menu/models.py
+++ nested_menu/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.urls import reverse, NoReverseMatch
 
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=255)
+    url_type = models.CharField(max_length=20, choices=[('url', 'URL'), ('named_url', 'Named URL')])
     url = models.CharField(max_length=255)
     parent = models.ForeignKey(
         'self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
@@ -12,3 +14,15 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.url = self.get_url()
+
+    def get_url(self):
+        if self.url_type == 'named_url':
+            try:
+                return reverse(self.url)
+            except NoReverseMatch:
+                return ''
+        return self.url
