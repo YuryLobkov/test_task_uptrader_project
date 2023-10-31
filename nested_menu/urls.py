f0f84636497879be1e378bd622ef2ef5825792e3
Yury Lobkov <racoon.enot@gmail.com>
2023-10-31 07:38:45 -0300

    include example database to repo

--- nested_menu/urls.py
+++ nested_menu/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<item_name>', views.item_page, name='level1_page'),
     path('<item_name>/<item_name2>', views.item_page_level2, name='level2_page'),
     path('<item_name>/<item_name2>/<item_name3>', views.item_page_level3, name='level3_page'),
+    path('namedlevel1', views.item_page, name='named_level1_page'),
+    path('namedlevel1/namedlevel2', views.item_page_level2, name='named_level2_page'),
 ]
